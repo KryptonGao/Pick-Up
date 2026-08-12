f82f37696978ded27735e21e140fad34008ac176
Gao-Chenkai <DevChenkai@outlook.com>
2026-08-12 21:18:53 +0800

    Initial Commit

---
+++ Pick Up/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pick Up
+//
+//  Created by Chenkai Gao on 2026/8/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pick Up/Pick_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_UpApp.swift
+//  Pick Up
+//
+//  Created by Chenkai Gao on 2026/8/12.
+//
+
+import SwiftUI
+
+@main
+struct Pick_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
